be7ad7333c97699ee2273137592e22ad3a8d83c0
DMoymy <addahy.om@gmail.com>
2021-05-12 13:35:58 -0600

    First comment

---
+++ src/com/momyProj/Driver1.java
@@ -0,0 +1,17 @@
+package com.momyProj;
+import com.moymyProj.bean.Driver2;
+
+public class Driver1 {
+	
+	 static void myStaticMethod() {
+		    System.out.println("Static methods");
+		  }
+	 
+	 
+	public static void main(String[] args) {
+	System.out.println("Practicng Java classes");
+	myStaticMethod(); // Call the static method
+	
+	}
+
+}
---
+++ src/com/moymyProj/bean/Driver2.java
@@ -0,0 +1,16 @@
+package com.moymyProj.bean;
+
+
+public class Driver2 {
+	
+	String name ="james";
+	
+	public void setName(String name){
+		   this.name = name;
+	   }
+	   
+	   public String getName() {
+		   return name;
+	   }
+
+}
